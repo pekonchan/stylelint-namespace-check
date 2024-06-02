9f67b1903fda89f330acba6cea39feabb17f3d6d
pekonchan <pekonchan@163.com>
2024-06-02 23:29:08 +0800

    fist commit

---
+++ test/example.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="ns-valid-class">
+      <div class="invalid-class">Hello World</div>
+    </div>
+  </template>
+  
+  <style>
+  .css {
+    color: blue;
+  }
+  </style>
+  
+  <style lang="less">
+  .less {
+    color: blue;
+  }
+  </style>
+  
+  <style lang="scss">
+  .scss {
+    color: blue;
+  }
+  </style>
+  
